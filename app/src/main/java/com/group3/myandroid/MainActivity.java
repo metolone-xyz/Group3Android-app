8b9b9e4e297aa5c149b1b7e89bc7729d85ad1f5c
metolone <metoronexyz@gmail.com>
2023-10-04 15:56:44 +0900

    little change

--- app/src/main/java/com/group3/myandroid/MainActivity.java
+++ app/src/main/java/com/group3/myandroid/MainActivity.java
@@ -1,16 +1,16 @@
 package com.group3.myandroid;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String test = "THIS_IS_TEST";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
