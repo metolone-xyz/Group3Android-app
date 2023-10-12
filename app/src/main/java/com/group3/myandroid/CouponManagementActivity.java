795a9e5b615d8938c9253f090da90b327581182b
metolone <metoronexyz@gmail.com>
2023-10-12 22:29:02 +0900

    Added CouponManagementActivity

---
+++ app/src/main/java/com/group3/myandroid/CouponManagementActivity.java
@@ -0,0 +1,13 @@
+package com.group3.myandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CouponManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupon_management);
+    }
+}
