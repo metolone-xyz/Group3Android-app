47cfcdabb5f4c068d4777c37dc85238ec198618f
metolone <metoronexyz@gmail.com>
2023-10-18 14:49:31 +0900

    mino3テスト

--- app/src/main/java/com/group3/myandroid/CouponManagementActivity.java
+++ app/src/main/java/com/group3/myandroid/CouponManagementActivity.java
@@ -2,6 +2,7 @@ package com.group3.myandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.group3.myandroid.global.EasyLogger;
 
 public class CouponManagementActivity extends AppCompatActivity {
 
@@ -10,4 +11,5 @@ public class CouponManagementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coupon_management);
     }
+    EasyLogger el = new EasyLogger("TestMode",true);
 }
