6bc3cc6a6bac849a54d13a23685b262e7f32c145
metolone <metoronexyz@gmail.com>
2023-10-25 14:02:30 +0900

    ManagementActivity.javaに歩数を記録するためのメソッド、変数を追加

--- app/src/main/java/com/group3/myandroid/MeasurementActivity.java
+++ app/src/main/java/com/group3/myandroid/MeasurementActivity.java
@@ -9,6 +9,10 @@ import android.os.Bundle;
 import com.group3.myandroid.global.EasyLogger;
 import org.w3c.dom.Text;
 import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
 //メモ:ボタンをインポートする
 
@@ -18,6 +22,12 @@ public class MeasurementActivity extends AppCompatActivity {
     private Handler handler = new Handler();
     private TextView timerTextView;
     private long elapsedTime = 0;
+
+    //歩数カウント関係の変数
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+    private int stepCount = 0;
+
     private Runnable updateTimeRunnable = new Runnable() {
 
         @Override
@@ -28,6 +38,20 @@ public class MeasurementActivity extends AppCompatActivity {
         }
     };
 
+    //歩数センサーのリスナー
+    private final SensorEventListener stepListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
+                //UIを更新する処理
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                //とりあえず無視
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +64,18 @@ public class MeasurementActivity extends AppCompatActivity {
         startTime = System.currentTimeMillis();
         handler.postDelayed(updateTimeRunnable, 0);
 
+        //センサーを初期化
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        if(stepSensor == null){
+            //エラー処理
+        }
+
         startButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                //クリック時にMeasurementActivity
+                //クリック時にCouponMeasurementActivityに移動
                 Intent intent = new Intent(MeasurementActivity.this, CouponManagementActivity.class);
                 startActivity(intent);
 
@@ -57,11 +89,36 @@ public class MeasurementActivity extends AppCompatActivity {
 
     }
 
+    //アクティビティがユーザーとの対話に戻る直前に呼ばれる。アクティブな状態になるとすぐにセンサーのデータを受け取る
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (stepSensor != null) {
+            sensorManager.registerListener(stepListener, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    //アクティビティが全面から離れる直前に呼び出される。
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (stepSensor != null) {
+            sensorManager.unregisterListener(stepListener);
+        }
+    }
+
+    //時間を更新するためのメソッド
     private void updateTimeDisplay(long millis) {
         int seconds = (int) (millis / 1000) % 60;
         int minutes = (int) ((millis / (1000*60)) % 60);
         int hours   = (int) ((millis / (1000*60*60)) % 24);
         timerTextView.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+    }
+
+    //歩数を更新するためのメソッド
+    private void updateStepCount(){
+        TextView stepCountTextView = findViewById(R.id.stepCountTextView); //表示するUI
+        stepCountTextView.setText("Steps: " + stepCount);   //表示内容
     }
 
 }
