86581971a65d8520a5e94a662d33ff3e18dc8788
metolone <metoronexyz@gmail.com>
2023-11-08 16:05:49 +0900

    カウントダウンタイマーを追加

--- app/src/main/java/com/group3/myandroid/StartActivity.java
+++ app/src/main/java/com/group3/myandroid/StartActivity.java
@@ -2,11 +2,10 @@ package com.group3.myandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -16,22 +15,24 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        Button startButton = findViewById(R.id.StartButton);    //ボタンを参照
+        Button startButton = findViewById(R.id.StartButton); //ボタンを参照
+        startButton.setOnClickListener(v -> {
+            // 3秒間のカウントダウンを開始
+            new CountDownTimer(5000, 1000) {
+                public void onTick(long millisUntilFinished) {
+                    // 残り時間を秒単位でtitleTextに表示
+                    TextView titleText = findViewById(R.id.titleText);
+                    titleText.setText(String.valueOf((millisUntilFinished + 999) / 1000));
+                }
 
-        //タイマーシステム
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(StartActivity.this, MeasurementActivity.class);
-                startActivity(intent);
-            }
-        };
-
-        startButton.setOnClickListener(view -> {
-            //クリック時にMeasurementActivityのアクティビティを開始
-            Timer timer = new Timer();
-            timer.schedule(task, 3000);
+                public void onFinish() {
+                    // カウントダウンが終了したら、MesurementActivityに遷移
+                    Intent intent = new Intent(StartActivity.this, MeasurementActivity.class);
+                    startActivity(intent);
+                }
+            }.start();
         });
+
     }
 
 
